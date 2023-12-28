592090ad28f5706823c34241784412a28d2b9ca7
DOJO <kevin11272@gmail.com>
2023-12-28 20:40:51 +0800

    create set todo list to localStorage

--- src/components/todoList/index.tsx
+++ src/components/todoList/index.tsx
@@ -1,14 +1,65 @@
-import { useState } from "react";
-import TodoInput from "../ui/todoInput";
-import TodoItem from "../ui/todoItem";
+import { FormEvent, useEffect, useState } from 'react';
+import TodoInput from '../ui/todoInput';
+import TodoItem from '../ui/todoItem';
 
 const TodoList = () => {
-  const [todo, setTodo] = useState<string>("");
+  const [todo, setTodo] = useState<string>('');
+  const [todoList, setTodoList] = useState<string[]>(() => {
+    const storedTodoList = localStorage.getItem('todoList');
+    return storedTodoList ? JSON.parse(storedTodoList) : [];
+  });  
+  const [finish, setFinish] = useState<string[]>([])
+
+  const addTodo = (e: FormEvent) => {
+    e.preventDefault();
+    setTodoList((prev) => prev.concat(todo));
+    setTodo('');
+  };
+
+  const checkedTodo = (item:string)=>{
+    if(finish.includes(item)){
+      setFinish((prev) => prev.filter((finishItem) => finishItem !== item));
+    }else{
+      setFinish((prev)=> [...prev, item])
+    }
+  }
+
+  const deleteTodo=(items:string)=>{
+    setTodoList((prev)=>prev.filter((deleteItem)=> deleteItem!= items))
+    setLocalStorage()
+  }
+
+  const setLocalStorage = ()=>{
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+  }
+
+  const getLocalStorage = ()=>{
+    const item = localStorage.getItem("todoList")
+    if(item){
+      setTodoList(JSON.parse(item))
+    }
+  }
+
+  // 從 localStorage 取得儲存的 todoList
+  useEffect(()=>{
+    getLocalStorage()
+  },[])
+
+  // 每次 todoList 更新時，將其保存到 localStorage 中
+  useEffect(() => {
+    setLocalStorage()
+  }, [todoList]);
 
   return (
     <>
-      <TodoInput todo={todo} setTodo={setTodo} />
-      <TodoItem />
+      <p className='text-4xl font-bold text-center'>TODO LIST</p>
+      <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
+      <TodoItem 
+      todoList={todoList}
+      checkedTodo={checkedTodo}
+      finish={finish}
+      deleteTodo={deleteTodo}
+       />
     </>
   );
 };
--- src/components/ui/todoItem.tsx
+++ src/components/ui/todoItem.tsx
@@ -1,9 +1,29 @@
-const TodoItem = () => {
+interface todoItemProps {
+  todoList: string[];
+  checkedTodo: (item: string) => void
+  finish: string[]
+  deleteTodo: (item:string)=>void
+}
+
+const TodoItem = ({ todoList, checkedTodo, finish,deleteTodo }: todoItemProps) => {
   return (
-    <div className="border m-4 p-2 flex items-center rounded-md shadow-sm placeholder max-h-12">
-      <input type="checkbox" />
-      <p className="mx-2">content</p>
-    </div>
+    <>
+      {todoList.map((items) => (
+        <div
+          className={`flex items-center p-2 m-4 border rounded-md shadow-sm placeholder relative  ${finish.includes(items) ? "opacity-30" : ""}`}
+          key={items}>
+          <input type='checkbox' onClick={() => checkedTodo(items)} />
+          <p className='mx-2'>{items}</p>
+          <img
+            src="/src/assets/img/bin.png"
+            alt="bin"
+            width={20}
+            className="absolute cursor-pointer right-5"
+            onClick={()=>deleteTodo(items)}
+            />
+        </div>
+      ))}
+    </>
   );
 };
 export default TodoItem;
